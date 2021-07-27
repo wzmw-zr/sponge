0f2abf7f95e3432e8ba5fcc563e30ada70dbc1f3
wzmw-zr <1767153298@qq.com>
2021-07-28 03:16:13 +0800

    simply  code.

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,51 +11,30 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    TCPHeader header = seg.header();
-    Buffer payload = seg.payload();
-    string data = payload.copy();
-    WrappingInt32 seqno = header.seqno;
-    bool syn = header.syn;
-    bool fin = header.fin;
     ByteStream &output = stream_out();
     if (_state == s_listen) {
-        if (!syn) return ;
-        _isn = seqno;
-        _offset = static_cast<uint64_t>(1);
-        seqno = seqno + static_cast<uint32_t>(1);
-        uint64_t checkpoint = static_cast<uint64_t>(output.bytes_written());
-        uint64_t index = unwrap(seqno, _isn, checkpoint) - _offset;
-        _reassembler.push_substring(data, index, fin);
-        if (fin) {
-            _state = s_fin;
-            _offset = static_cast<uint64_t>(2);
-        } else {
-            _state = s_syn_recv;
-        }
+        if (!seg.header().syn) return ;
+        _isn = seg.header().seqno;
+        _reassembler.push_substring(seg.payload().copy(), 0, seg.header().fin);
+        if (output.input_ended()) _state = s_fin;
+        else _state = s_syn_recv;
     } else if (_state == s_syn_recv) {
-        if (syn) return ;
-        uint64_t checkpoint = static_cast<uint64_t>(output.bytes_written());
-        uint64_t index = unwrap(seqno, _isn, checkpoint) - _offset;
-        // uint64_t index = unwrap(seqno, _isn, abs_seqno);
-        _reassembler.push_substring(data, index, fin);
-        if (output.input_ended()) {
-            _state = s_fin;
-            _offset = static_cast<uint64_t>(2);
-        }
+        if (seg.header().syn) return ;
+        uint64_t checkpoint = output.bytes_written();
+        uint64_t index = unwrap(seg.header().seqno, _isn, checkpoint) - 1;
+        _reassembler.push_substring(seg.payload().copy(), index, seg.header().fin);
+        if (output.input_ended()) _state = s_fin;
     } else if (_state == s_fin) {}
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { 
     if (_state == s_listen) return optional<WrappingInt32>{nullopt};
-    const ByteStream &output = stream_out();
-    uint64_t abs_sqeno = static_cast<uint64_t>(output.bytes_written()) + _offset;
-    return optional<WrappingInt32>{wrap(abs_sqeno, _isn)};
+    return optional<WrappingInt32>{wrap(stream_out().bytes_written() + (_state == s_syn_recv ? 1 : 2), _isn)};
 }
 
 size_t TCPReceiver::window_size() const { 
-    const ByteStream &output = stream_out();
-    size_t unread = output.bytes_read();
-    size_t unassembled = output.bytes_written();
+    size_t unread = stream_out().bytes_read();
+    size_t unassembled = stream_out().bytes_written();
     size_t unacceptable = unread + _capacity;
     return unacceptable - unassembled;
 }
